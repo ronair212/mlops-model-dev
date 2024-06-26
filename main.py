d1f6657bf5695fe369b2ad219572d7acd930a54e
Kunal18 <kunalindore18@gmail.com>
2024-06-25 21:30:03 -0400

    hyperparameter tuning code changes

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ try:
    logger.info(f"*******************")
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    model_evalution = EvaluationPipeline()
-   model_evalution.main(xgb)
+   model_evalution.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
    logger.exception(e)
@@ -62,7 +62,7 @@ try:
    logger.info(f"*******************")
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    model_tracking = MLFlowTrackingPipeline()
-   model_tracking.main(xgb)
+   model_tracking.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
    logger.exception(e)
