6e47bc350869bab21408092bc73cf8122a180af8
ronair212 <rohitnair212@gmail.com>
2024-06-27 01:44:39 -0400

    updated files

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -3,7 +3,18 @@ import os
 
 def upload_to_s3(file_name, bucket, object_name=None):
     """Upload a file to an S3 bucket"""
-    s3_client = boto3.client('s3')
+    # Read AWS credentials from environment variables
+    aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+    aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+    aws_region = os.getenv('AWS_REGION')
+
+    s3_client = boto3.client(
+        's3',
+        region_name=aws_region,
+        aws_access_key_id=aws_access_key_id,
+        aws_secret_access_key=aws_secret_access_key
+    )
+    
     try:
         response = s3_client.upload_file(file_name, bucket, object_name or file_name)
     except Exception as e:
@@ -14,8 +25,8 @@ def upload_to_s3(file_name, bucket, object_name=None):
 if __name__ == "__main__":
     processed_data_file = 'artifacts/data_ingestion/card_transdata.csv'
     model_file = 'artifacts/training/model.pkl'
-    bucket_name = 'mlops-dev-s3'
+    bucket_name = os.getenv('AWS_S3_BUCKET_NAME')  # Read bucket name from environment variable
     
     # Upload files
-    upload_to_s3(processed_data_file, bucket_name, 'processed_data.csv')
-    upload_to_s3(model_file, bucket_name, 'model.pkl')
+    upload_to_s3(processed_data_file, bucket_name, os.getenv('AWS_S3_OBJECT_NAME_PROCESSED', 'processed_data.csv'))
+    upload_to_s3(model_file, bucket_name, os.getenv('AWS_S3_OBJECT_NAME_MODEL', 'model.pkl'))
