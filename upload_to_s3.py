0c213794e967effff7423d87d99781d53ffaa697
ronair212 <rohitnair212@gmail.com>
2024-06-27 01:50:06 -0400

    updated files

--- upload_to_s3.py
+++ upload_to_s3.py
@@ -8,6 +8,14 @@ def upload_to_s3(file_name, bucket, object_name=None):
     aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
     aws_region = os.getenv('AWS_REGION')
 
+    # Debug prints
+    print(f"AWS_ACCESS_KEY_ID: {aws_access_key_id}")
+    print(f"AWS_SECRET_ACCESS_KEY: {aws_secret_access_key}")
+    print(f"AWS_REGION: {aws_region}")
+    print(f"Bucket: {bucket}")
+    print(f"File name: {file_name}")
+    print(f"Object name: {object_name or file_name}")
+
     s3_client = boto3.client(
         's3',
         region_name=aws_region,
@@ -27,6 +35,9 @@ if __name__ == "__main__":
     model_file = 'artifacts/training/model.pkl'
     bucket_name = os.getenv('AWS_S3_BUCKET_NAME')  # Read bucket name from environment variable
     
+    # Debug prints
+    print(f"Bucket Name: {bucket_name}")
+
     # Upload files
     upload_to_s3(processed_data_file, bucket_name, os.getenv('AWS_S3_OBJECT_NAME_PROCESSED', 'processed_data.csv'))
     upload_to_s3(model_file, bucket_name, os.getenv('AWS_S3_OBJECT_NAME_MODEL', 'model.pkl'))
